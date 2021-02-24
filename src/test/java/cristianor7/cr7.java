7fc71621bdfedcff7752b05aebae7ecad24187eb
Dany1407 <danielsam125@gmail.com>
2021-02-24 22:37:04 +0530

    new

---
+++ src/test/java/cristianor7/cr7.java
@@ -0,0 +1,27 @@
+package cristianor7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.WebElement;
+
+
+public class cr7 extends baseclasssss{
+public static void main(String[] args) {
+	lauchbrower();
+	lauchurl("https://www.facebook.com/");
+	loginssssss f=new loginssssss();
+	userlogin(f.getUsername(), "kjjghff");
+driver.navigate().refresh();
+
+
+
+userlogin(f.getUsername()," value");
+	userlogin(f.getUserpass(), "vicky");
+btn(f.getButton());	
+	
+   
+   
+}
+
+
+}
